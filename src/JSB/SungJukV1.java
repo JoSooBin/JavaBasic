4f16c491644ebf49caa46e7cee42d47225905c03
JoSooBin <ssss6756@naver.com>
2020-11-17 11:45:56 +0900

    선생님이 작성한 성적V1

--- src/JSB/SungJukV1.java
+++ src/JSB/SungJukV1.java
@@ -9,25 +9,39 @@ package JSB;
  * name, kor, eng, mat, sum, mean, grd
  * 학점 기준은 수우미양가로 함
  * 100-90, 89-80....
+ *
+ * 삼항연산자 : (조건식) ? : 참일때의 값: 거짓일 때 값
  */
 public class SungJukV1 {
     public static void main(String[] args) {
-        String name = "조수빈";
-        int kor=99, eng=98, mat=99;
-        double sum = kor + eng + mat;
-        double mean = sum/3;
+        //변수선언
+        String name = "조수빈";   //한글자:char =''
+        int kor = 99;
+        int eng = 98;
+        int mat = 99;
+        int sum = 0;
+        double mean = 0.0;
+        char grd = '가';
 
-        /*if(mean>=100 && mean>90){
-            System.out.println("학점은 '수'입니다");
-        }*/
+        // 처리
+        sum = mat + kor + eng;
+        mean = (double)sum/3;
 
-        System.out.println("이름 : "+ name);
-        System.out.println("국어 : "+ kor);
-        System.out.println("영어 : "+ eng);
-        System.out.println("수학 : "+ mat);
+        //삼항연산자
+        grd = (mean >= 90) ? '수':
+              (mean >= 80) ? '우':
+              (mean >= 70) ? '미':
+              (mean >= 60) ? '양': '가';
+
+        //결과출력
+        System.out.println("이름 : " + name);
+        System.out.println("국어 : " + kor);
+        System.out.println("영어 : " + eng);
+        System.out.println("수학 : " + mat);
         System.out.println("-----------");
-        System.out.println("총점 : "+ sum);
-        System.out.println("평균 : "+ mean);
-        //System.out.println("학점은"+mat+"입니다");
+        System.out.println("총점 : " + sum);
+        System.out.println("평균 : " + mean);
+        System.out.println("학점 : " + grd);
+
     }
 }
