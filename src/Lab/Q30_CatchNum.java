bfc74b35cf82128ffb729e04e2d324b382f07d04
JoSooBin <ssss6756@naver.com>
2020-11-18 18:21:33 +0900

    29번 소문자를 대문자로 do~while

--- src/Lab/Q30_CatchNum.java
+++ src/Lab/Q30_CatchNum.java
@@ -8,17 +8,15 @@ public class Q30_CatchNum {
         System.out.print("1~999 중 번호 하나를 입력하세요 : ");
         int num1 = sc.nextInt();
         int num2 = (int)(Math.random() * 999) + 1;
-        System.out.println(num2);
-        switch (num1>=1 && num1<100){
-            case num1>num2 :
+        System.out.println("랜덤으로 추출된 숫자는"+num2);
+        do{
+            if(num1>num2){
                 System.out.println("추측한 숫자가 큽니다");
                 break;
-            case num1<num2 :
+            }else if(num1<num2){
                 System.out.println("추측한 숫자가 작습니다");
                 break;
-            default:
-                System.out.println("빙고! 숫자를 맞췄습니다");
-                break;
-        };
+            }else System.out.println("빙고! 숫자를 맞췄습니다");
+        }while (num1>=1 && num1<100);
     }
 }
