f1ee2030d3eadbc80c6f05c798e69801e5579fea
JoSooBin <ssss6756@naver.com>
2020-11-19 17:53:22 +0900

    선생님이 작성한 Q79 감독

--- src/Lab/movies/Director.java
+++ src/Lab/movies/Director.java
@@ -1,5 +1,6 @@
 package Lab.movies;
-
+//감독들도 이름과 생년월일로 식별되며 한 개 이상의 영화를 감독한다.
+// 감독이 영화에 출현하는 것도 가능하다. (그 감독이 감독한 영화에 한해서 말이다)
 public class Director {
     //멤버변수
     private String name;
---
+++ src/Lab/movies/Producer.java
@@ -0,0 +1,9 @@
+package Lab.movies;
+//제작사는 이름으로 식별되며 주소를 갖는다. 제작사는 한 개 이상의 영화를 제작한다
+public class Producer {
+
+    private String name;
+    private String addr;
+    private String[] movie;
+
+}
