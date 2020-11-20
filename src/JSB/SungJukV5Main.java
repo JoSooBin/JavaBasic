782e62a5ee4ee8299bffd2515768373bb641d7aa
JoSooBin <ssss6756@naver.com>
2020-11-20 13:01:17 +0900

    선생님이 작성한 SungJukV5. SungJukService, SungJukV5Main

--- src/JSB/SungJukV5Main.java
+++ src/JSB/SungJukV5Main.java
@@ -6,7 +6,8 @@ public class SungJukV5Main {
 
         SungJukService sjsrv = new SungJukService(); //기능만 담고있는 클래스
 
-        SungJukVO  sj = sjsrv.readSungJuk();
+        //SungJukVO sj = new SungJukVO(); // 객체 생성할때 원래 쓰던거
+        SungJukVO  sj = sjsrv.readSungJuk(); //9번줄을 캡슐화
 
         //변수의 유효범위scope
         //변수가 생성된 위치와 사용하는 위치가 다른경우
@@ -14,7 +15,10 @@ public class SungJukV5Main {
         //변수의 유효범위는 블록이라하고 { }로 표현
         //기본자료형 변수 : call by value
         //참조자료형 변수 : call by reference
+
+        //학점계산
         sjsrv.computeSungJuk(sj);
+        //출력
         sjsrv.printSungJuk(sj);
     }
 }
