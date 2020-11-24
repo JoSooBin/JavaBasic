d10e0ca5f1c771da9d300dc154584f0c760fdcd7
JoSooBin <ssss6756@naver.com>
2020-11-24 15:40:06 +0900

    문자열처리 유용한 기능을 제공하는 메서드(선생님이 작성한 주민번호 성별찾기2)

--- src/JSB/Strings2.java
+++ src/JSB/Strings2.java
@@ -57,13 +57,15 @@ public class Strings2 {
 
         String jumin = "123456-1234567";
         String gender = jumin.substring(7,8);
+        String result2 = "";
 
         switch (gender) {
-            case  "1" : System.out.println("2000년 이전 출생 남자"); break;
-            case  "2" : System.out.println("2000년 이전 출생 여자"); break;
-            case  "3" : System.out.println("2000년 이후 출생 남자"); break;
-            case  "4" : System.out.println("2000년 이후 출생 여자"); break;
+            case  "1" : result2 = "2000년 이전 출생 남자"; break;
+            case  "2" : result2 = "2000년 이전 출생 여자"; break;
+            case  "3" : result2 = "2000년 이후 출생 남자"; break;
+            case  "4" : result2 = "2000년 이후 출생 여자"; break;
         }
+        System.out.println(result2);
 
     }
 }
