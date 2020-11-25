844798b5a030a4101bcc9c9a7ac191c5164df479
JoSooBin <ssss6756@naver.com>
2020-11-25 10:55:51 +0900

    지네릭Genercic / 컬렉션 프레임워크

---
+++ src/JSB/ArrayLists.java
@@ -0,0 +1,13 @@
+package JSB;
+
+//ArrayList
+//동적 배열의 한 종류
+//Lis 인터페이스를 구현해서 만든 컬렉션 프레임워크
+//요소의 저장 순서가 유지
+//중복으로 데이터를 저장 할 수 없음
+//크키가 늘어나면 새로운 배열을 생성하고 기존의 요소들을 옮겨야하는 복잡한 과정이 동반됨.
+public class ArrayLists {
+    public static void main(String[] args) {
+
+    }
+}
