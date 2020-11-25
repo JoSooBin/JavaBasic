3d60e0ecab10c1faed602bb4d049aa9ae8b98e71
JoSooBin <ssss6756@naver.com>
2020-11-25 11:03:21 +0900

    ArrayList / for(String name : names)

--- src/JSB/ArrayLists.java
+++ src/JSB/ArrayLists.java
@@ -1,13 +1,25 @@
 package JSB;
 
+import java.util.ArrayList;
+
 //ArrayList
 //동적 배열의 한 종류
-//Lis 인터페이스를 구현해서 만든 컬렉션 프레임워크
+//List 인터페이스를 구현해서 만든 컬렉션 프레임워크
 //요소의 저장 순서가 유지
-//중복으로 데이터를 저장 할 수 없음
+//중복으로 데이터를 저장 할 수 있음
 //크키가 늘어나면 새로운 배열을 생성하고 기존의 요소들을 옮겨야하는 복잡한 과정이 동반됨.
 public class ArrayLists {
     public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
 
+        //데이터 추가 : add(대상)
+        names.add("혜교");
+        names.add("지현");
+        names.add("수지");
+
+        //컬렉션 자료구조에 저장된 데이터를 출력하려면 foreach 구문 사용
+        for(String name : names) {
+            System.out.println(name);
+        }
     }
 }
