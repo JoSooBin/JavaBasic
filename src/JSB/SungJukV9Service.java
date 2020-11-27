aa9ee55a70aacd26c3aec23f9e0862fad60dbcce
JoSooBin <ssss6756@naver.com>
2020-11-27 15:49:16 +0900

    SungJukV9 입력받은 성적데이터 누적 저장

--- src/JSB/SungJukV9Service.java
+++ src/JSB/SungJukV9Service.java
@@ -11,6 +11,15 @@ public class SungJukV9Service extends SungJukV8Service {
     private BufferedReader br = null;
     private BufferedWriter bw = null;
 
+    public SungJukV9Service() {
+        //프로그램 최소 시작시 파일에 저장된 모든 성적데이터를 List타입의 sjdata변수에 저장
+        try {
+            loadSungJuk();
+        } catch (IOException e) {
+            System.out.println("성적데이터 읽어오는 중 오류 발생");
+        }
+    }
+
     public void displayMenu(){
         StringBuilder sb = new StringBuilder();
         sb.append("--------------------\n")
@@ -30,28 +39,8 @@ public class SungJukV9Service extends SungJukV8Service {
     //SungJukV8SerVice의 newSungJuk를 재정의
     @Override
     public void newSungJuk() {
-        String name;
-        int kor, eng, mat;
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("이름을 입력하세요 : ");
-        name = sc.nextLine();
-        System.out.print("국어점수를 입력하세요 : ");
-        kor = sc.nextInt();
-        System.out.print("영어점수를 입력하세요 : ");
-        eng = sc.nextInt();
-        System.out.print("수학점수를 입력하세요 : ");
-        mat = sc.nextInt();
-
-        //입력받은 성적데이터를 동적배열에 저장
-        SungJukVO sj = new SungJukVO(name, kor, eng, mat,0,0,' ');
-
-        //총점, 평균, 학점을 계산
-        computeSungJuk(sj);
-
-        //처리된 성적데이터를 동적배열에 저장
-        sjdata.add(sj);
+        //성적데이터 입력받은 후 arrayList 객체에 저장
+        super.newSungJuk();
 
         try {
             writeSungJuk(sj);
@@ -68,13 +57,46 @@ public class SungJukV9Service extends SungJukV8Service {
     protected void writeSungJuk(SungJukVO sj) throws IOException {
         String fmt = "%s,%s,%s,%s,%s,%.1f,%s";
 
-        fw = new FileWriter(fpath);
+        //파일 기록시 추가append 기능을 활성화 함
+        fw = new FileWriter(fpath, true);
         bw = new BufferedWriter(fw);
 
-        String data = String.format(fmt,sj.getName(),sj.getKor(),sj.getEng(),sj.getMat(),sj.getSum(),sj.getMean(),sj.getGrd());
+        String data = String.format(fmt, sj.getName(),sj.getKor(),sj.getEng(),sj.getMat(),sj.getSum(),sj.getMean(),sj.getGrd());
+
         bw.write(data);
+        //FileWriter 클래스를 이용해서 파일에 데이터를 기록할 때 다소 불편함
+        //=> PrintWriter 추천!
+        bw.newLine();//줄바꿈 메서드
 
         bw.close();
         fw.close();
+    }
+
+    /*
+    * readloadJuk
+    * 파일에 저장된 성적데이터를 List타입의 sjdata에 저장
+    *
+    * */
+    protected void loadSungJuk() throws IOException {
+        fr = new FileReader(fpath);
+        br = new BufferedReader(fr);
+
+        while (br.ready()){
+            String[] s = br.readLine().split(",");
+
+            sj = new SungJukVO(s[0],
+                    Integer.parseInt(s[1]),
+                    Integer.parseInt(s[2]),
+                    Integer.parseInt(s[3]),
+                    Integer.parseInt(s[4]),
+                    Double.parseDouble(s[5]),
+                    s[6].charAt(0)
+                    );
+
+            //파일에서 읽어온 성적 데이터 하나를 List변수 sjdata에 추가함
+            sjdata.add(sj);
+        }
+        br.close();
+        fr.close();
     }
 }
