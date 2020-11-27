68cfb6feccdf051679e3bece3c5c1b5b71f5935f
JoSooBin <ssss6756@naver.com>
2020-11-27 12:20:53 +0900

    데이터 읽기. 읽어온 데이터를 , 단위로 분리해서 배열 변수에 저장 및 출력

--- src/JSB/FileReaderWriter2.java
+++ src/JSB/FileReaderWriter2.java
@@ -21,14 +21,14 @@ public class FileReaderWriter2 {
         //기본 정보를 저장 할 형식 정의
         // 각 항목들은 ,로 구분함
         String fmt = "%s,%s,%s,%s,%s,%s";
-        String data = String.format(fmt, hakbun, name, addr,birth,dname,prof);
+        String data = String.format(fmt, hakbun, name, addr, birth, dname, prof);
 
         fw.write(data);
         fw.close();
 
         //과목 정보 입력
         //과목번호, 과목명, 과목개요, 담당교수 등으로 구성된 2차원 표 형태의 데이터를 subject.dat에 저장
-        int num = 0205;
+        String num = "0205";
         String kname = "프로그래밍";
         String gaeyo = "자바 프로그래밍";
         prof = 301;
@@ -39,9 +39,31 @@ public class FileReaderWriter2 {
         BufferedWriter bw = new BufferedWriter(fw);
 
         fmt = "%s,%s,%s,%s";
-        data = String.format(fmt, num, kname, gaeyo,prof);
+        data = String.format(fmt, num, kname, gaeyo, prof);
 
         bw.write(data);
+
+        bw.close();
+        fw.close();
+
+        //과목 기본정보가 저장된 파일을 읽어서 출력하기
+        //과목번호 : ~~
+        //과목이름 : ~~
+        //...
+
+        FileReader fr = new FileReader(fpath);
+        BufferedReader br = new BufferedReader(fr);
+        fmt = "과목번호 : %s\n과목명 : %s\n과목개요 : %s\n담당교수 : %s\n";
+
+        //파일 데이터 읽기
+        while (br.ready()){
+            String line = br.readLine(); //데이터 읽기
+            //System.out.println(line);
+
+            String[] item = line.split(","); //읽어온 데이터를 , 단위로 분리해서 배열 변수에 저장
+            String result = String.format(fmt, item[0], item[1], item[2], item[3]) ;
+            System.out.println(result);
+        }
 
         bw.close();
         fw.close();
