a9766cafe56cb80bf0ca5f47c93be2ee941efe3f
Cho soo been <ssss6756@naver.com>
2020-12-15 18:00:06 +0900

    선생님이 쓴 JDBCMembers3, JDBCMembers3c

--- src/JSB/JDBCMembers3c.java
+++ src/JSB/JDBCMembers3c.java
@@ -6,21 +6,26 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-public class JDBCMembers3 {
+public class JDBCMembers3c {
     public static void main(String[] args) {
-        //bookmembers 모두출력
-
+        //김씨 성을 갖는 이름 조회
         Connection conn = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
 
-        String sql = "select * from bookmembers";
+        String sql = "select * from bookmembers where name like ?";
         String fmt = "%s %s %s %s\n";
         StringBuilder sb = new StringBuilder();
+
+        //조회할 이름 입력받음
+        Scanner sc = new Scanner(System.in);
+        System.out.println("회원의 성은?");
+        String sname = sc.nextLine();
 
         conn = JDBCUtil.makeConn();
         try {
             pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, sname+'%'); //책 제목 받기
             rs = pstmt.executeQuery(); // 결과받기
 
             while (rs.next()){
