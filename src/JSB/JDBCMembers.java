8cb1655c763d26d5e3467eb5b5d66f5118cff967
Cho soo been <ssss6756@naver.com>
2020-12-15 10:13:30 +0900

    선생님이 쓴JDBCMembers

--- src/JSB/JDBCMembers.java
+++ src/JSB/JDBCMembers.java
@@ -11,8 +11,14 @@ public class JDBCMembers {
     //컬럼정보 : custid, name, address, phone
     //입력은 Scanner 이용
     public static void main(String[] args) {
-        //1.도서정보 입력
-        Scanner sc = new Scanner(System.in);
+        //데이터베이스 연결정보
+        String DRV = "org.mariadb.jdbc.Driver";
+        String URL ="jdbc:mariadb://mariadb.czqb0qwcxo45.ap-northeast-2.rds.amazonaws.com:3306/playground";
+        String USR ="playground";
+        String PWD ="playground2020";
+
+        //1.회원원정보 입력
+       Scanner sc = new Scanner(System.in);
 
         System.out.println("회원번호는?  ");
         int custid = Integer.parseInt(sc.nextLine());
@@ -30,42 +36,32 @@ public class JDBCMembers {
         System.out.println(sql);
 
         //3. 생성한 SQL문을 JDBC를 통해 실행
-        String DRV = "org.mariadb.jdbc.Driver";
-        String URL ="jdbc:mariadb://mariadb.czqb0qwcxo45.ap-northeast-2.rds.amazonaws.com:3306/playground";
-        String USR ="playground";
-        String PWD ="playground2020";
 
-        //a) JDBC 드라이버 초기화
-        try {
-            Class.forName(DRV);
-        } catch (ClassNotFoundException cnf) {
-            System.out.println("JDBC 드라이버를 확인하세요!");
-        }
-
+        // JDBC 드라이버 초기화
+        // JDBC 커넥션 객체 생성
+        // JDBC Statement 객체 생성
+        // SQL문 실행 및 결과 확인
+        // JDBC관련 객체 제거
         Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(URL, USR, PWD);
-        } catch (SQLException se) {
-            System.out.println("디비 접속주소가 틀리거나 아이디/비번을 확인하세요");
-        }
-
-        // c) SQL문 객체를 생성하고 서버로 전송해서 실행함
         Statement stmt = null;
         try {
+            Class.forName(DRV);
+            conn = DriverManager.getConnection(URL, USR, PWD);
             stmt = conn.createStatement();
-            boolean isfail = stmt.execute(sql); //sql문 실행
-            if(!isfail) System.out.println("데이터 추가완료!"); //참이면 실행완료
-        } catch (SQLException se) {
-            System.out.println("JDBC - SQL 실행 오류");
+
+            int cnt = stmt.executeUpdate(sql);
+            if(cnt>0)
+                System.out.println("데이터 추가 완료!");
+            } catch (ClassNotFoundException cnf) {
+                System.out.println("JDBC 드라이버를 확인하세요!");
+            }catch (SQLException se) {
+                System.out.println("JDBC 연결실패!");
+            }finally {
+                if(stmt != null)
+                    try { stmt.close(); } catch (SQLException se) { }
+                if(conn != null)
+                    try { conn.close(); } catch (SQLException se) { }
         }
-
-        // d) JDBC 관련 객체는 메모리에서 제거
-        if(stmt != null)
-            try { stmt.close(); } catch (SQLException se) { }
-
-        if(conn != null)
-            try { conn.close(); } catch (SQLException se) { }
-
 
     }
 }
