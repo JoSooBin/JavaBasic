84e364df9af4ba32125257669ab045b4b26ed57e
Cho soo been <ssss6756@naver.com>
2020-12-16 10:06:22 +0900

    선생님이 쓴 JDBCOders3b

--- src/JSB/JDBCOders3b.java
+++ src/JSB/JDBCOders3b.java
@@ -13,26 +13,26 @@ public class JDBCOders3b {
         PreparedStatement pstmt = null;
         ResultSet rs = null;
 
-        String sql = "select * from bookmembers where orderid= ?";
-        String fmt = "%s %s %s %s\n";
+        String sql = "select * from bookorders where orderid= ?";
+        String fmt = "%s %s %s %s %s\n";
         StringBuilder sb = new StringBuilder();
 
-        //조회할 이름 입력받음
+        //조회할 번호 입력받음
         Scanner sc = new Scanner(System.in);
-        System.out.println("검색할 주문번호은?");
-        int sid = sc.nextInt();
+        System.out.print("검색할 주문번호은? ");
+        String sid = sc.nextLine();
 
         conn = JDBCUtil.makeConn();
         try {
             pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, sid); //책 제목 받기
+            pstmt.setInt(1,Integer.parseInt(sid));
             rs = pstmt.executeQuery(); // 결과받기
 
             while (rs.next()){
-                String orderid = String.format(rs.getString(1));
-                String custid = String.format(rs.getString(2));
-                String bookid = String.format(rs.getString(3));
-                String saleprice = String.format(rs.getString(4));
+                String orderid = rs.getString(1);
+                String custid = rs.getString(2);
+                String bookid = rs.getString(3);
+                String saleprice = rs.getString(4);
                 String orderdate = rs.getString(5);
                 String result = String.format(fmt,orderid,custid,bookid,saleprice,orderdate);
                 sb.append(result);
